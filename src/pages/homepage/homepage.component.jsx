b9065d3636e79dd09607c4530ec465a01d321a39
low-key <tsemibhutia@gmail.com>
2020-01-07 19:49:55 +0530

    added images and css effects to the homepage menu-directory

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss'
+import MenuDirectory from '../../components/menu-directory/menu-directory.component';
+
+const HomePage = () => (
+    <div className="homepage">
+        <MenuDirectory />
+    </div>
+);
+
+export default HomePage;
