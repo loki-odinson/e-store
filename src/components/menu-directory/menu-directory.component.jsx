20ac082ad2a09ce77fa0e3d55f84bf0361074115
low-key <tsemibhutia@gmail.com>
2020-01-09 01:31:01 +0530

    added routing capabilities to base HatsPage using withRouter HOC

--- src/components/menu-directory/menu-directory.component.jsx
+++ src/components/menu-directory/menu-directory.component.jsx
@@ -12,29 +12,34 @@ class MenuDirectory extends React.Component {
                 {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1
+                    id: 1,
+                    linkUrl: 'hats'
                 },
                 {
                     title: 'jackets',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2
+                    id: 2,
+                    linkUrl: ''
                 },
                 {
                     title: 'sneakers',
                     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3
+                    id: 3,
+                    linkUrl: ''
                 },
                 {
                     title: 'women\'s',
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'larger',
-                    id: 4
+                    id: 4,
+                    linkUrl: ''
                 },
                 {
                     title: 'men\'s',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.jpg',
                     size: 'larger',
-                    id: 5
+                    id: 5,
+                    linkUrl: ''
                 },
             ]
         }
@@ -43,8 +48,8 @@ class MenuDirectory extends React.Component {
     render(){
         return (
             <div className="menu-directory">
-                {this.state.sections.map(({title, imageUrl, size, id}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                {this.state.sections.map(({id, ...otherSectionsProps}) => (
+                    <MenuItem key={id} {...otherSectionsProps} />
                 ))}
             </div>
         );
